e877c047c811ee2a8f48bb43d121ca2a9c8e6bec
Andreea P <andreeapatrascu1997@gmail.com>
2020-01-12 00:20:20 +0200

    create table useri

--- Client/client.c
+++ Client/client.c
@@ -7,17 +7,17 @@
 #include <stdlib.h>
 #include <netdb.h>
 #include <string.h>
-
+#include <arpa/inet.h>
 /* codul de eroare returnat de anumite apeluri */
 extern int errno;
 
+
 /* portul de conectare la server*/
 int port;
-
 int main (int argc, char *argv[])
 {
   int sd;			// descriptorul de socket
-  struct sockaddr_in server;	// structura folosita pentru conectare 
+  struct sockaddr_in server;	// structura folosita pentru conectare
   		// mesajul trimis
   int nr=0;
   char buf[10];
@@ -46,40 +46,136 @@ int main (int argc, char *argv[])
   server.sin_addr.s_addr = inet_addr(argv[1]);
   /* portul de conectare */
   server.sin_port = htons (port);
-  
+
   /* ne conectam la server */
   if (connect (sd, (struct sockaddr *) &server,sizeof (struct sockaddr)) == -1)
-    {
+  {
       perror ("[client]Eroare la connect().\n");
       return errno;
+  }
+
+  // Console shopper
+  int operatie;
+  char nume[50], parola[50];
+  printf("Console shopper.\n");
+  printf("1.Login.\n");
+  printf("2.Sign UP.\n");
+
+  // Login/Sign Up
+  scanf("%d", &operatie);
+
+  if (operatie == 1) {
+    // Trimitem la server operatia
+    write(sd, &operatie, 4);
+    printf("User :"); scanf("%s", nume);
+    printf("Parola :"); scanf("%s", parola);
+    //printf("%s %s", nume, parola);
+    int len1 = strlen(nume);
+    int len2 = strlen(parola);
+    //printf("%d %d", len1, len2);
+    fflush(stdout);
+    write(sd, &len1, 4);
+    write(sd, nume, len1);
+    write(sd, &len2, 4);
+    write(sd, parola, len2);
+
+    int ok;
+    read(sd, &ok, 4);
+
+    if (ok == 1) {
+      printf("Logat cu succes.\n\n\n");
+      printf("\n");
+    } else {
+      printf("Mai incearca.");
+      printf("\n");
+      return(0);
+    }
+  }
+
+  if (operatie == 2) {
+    write(sd, &operatie, 4);
+    printf("User :"); scanf("%s", nume);
+    printf("Parola :"); scanf("%s", parola);
+    int len1 = strlen(nume);
+    int len2 = strlen(parola);
+    fflush(stdout);
+    write(sd, &len1, 4);
+    write(sd, nume, len1);
+    write(sd, &len2, 4);
+    write(sd, parola, len2);
+    int ok;
+    read(sd, &ok, 4);
+
+    if (ok == 0){
+      printf("Inregistrat cu succes.\n\n\n");
+      printf("\n");
+    } else {
+      printf("Account folosit..");
+      printf("\n");
+      return(0);
+    }
+  }
+  int totSum = 0;
+  int vector[200];
+  int g = 1;
+
+  while(g) {
+    g = 0;
+    char categorii[5000];
+    memset(categorii, 0, sizeof(categorii));
+    int len3;
+    read(sd, &len3, 4);
+    read(sd, categorii, len3);
+    printf("%s",categorii);
+    printf("\n\n");
+    int cat;
+    printf("Alege categoria: "); scanf("%d", &cat);
+    // Trimitem la server categoria
+    write(sd, &cat, 4);
+    int n;
+    char c[50][50];
+    char p[50][50];
+    // Serverul ne intoarce numarul de categorii
+    read(sd, &n, 4);
+
+    // Serverul intoarce vectorii cu produsele
+    read(sd, p, 2500);
+    read(sd, c, 2500);
+    printf("N = %d\n\n", n);
+
+    // Iteram peste produse
+    for(int i=0;i<n;i++){
+        printf("%d. %s -> %s RONI \n", i + 1, p[i], c[i]);
     }
 
-  /* citirea mesajului */
-  printf ("[client]Introduceti un numar: ");
-  fflush (stdout);
-  read (0, buf, sizeof(buf));
-  nr=atoi(buf);
-  //scanf("%d",&nr);
-  
-  printf("[client] Am citit %d\n",nr);
+    // Golim vectorul
+    memset(vector, 0, sizeof(vector));
 
-  /* trimiterea mesajului la server */
-  if (write (sd,&nr,sizeof(int)) <= 0)
-    {
-      perror ("[client]Eroare la write() spre server.\n");
-      return errno;
+    int x = 1;
+
+    while(x) {
+       printf("Selecteaza item-ul pe care vrei sa il adaugi in cos. Tastezi 0 cand termini. \n");
+       scanf("%d", &x);
+       vector[x]++;
+       printf("A fost adaugat item-ul %d in cos. \n\n", x);
     }
+    printf("\n----------------------------------------------\n");
+    int sum = 0;
 
-  /* citirea raspunsului dat de server 
-     (apel blocant pina cind serverul raspunde) */
-  if (read (sd, &nr,sizeof(int)) < 0)
-    {
-      perror ("[client]Eroare la read() de la server.\n");
-      return errno;
+    for(int i = 1; i < 50; i++){
+      if (vector[i]) {
+          sum += atoi(c[i-1]) * vector[i];
+          printf("Elementul %d este de %d ori in cos.\n\n", i, vector[i]);
+      }
     }
-  /* afisam mesajul primit */
-  printf ("[client]Mesajul primit este: %d\n", nr);
+    printf("-----------------------------------------------");
+    printf("\nSuma  produselor alese din categoria asta = %d RONI\n\n\n", sum);
+    totSum += sum;
+    printf("0.Checkout.\n");
+    printf("1.Alege produse din alta categorie.\n");
+    scanf("%d", &g);
+    write(sd, &g, 4);
+ }
 
-  /* inchidem conexiunea, am terminat */
-  close (sd);
+  printf("Total spre plata : %d\n", totSum);
 }
--- Server/server.c
+++ Server/server.c
@@ -8,71 +8,29 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <pthread.h>
-
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pwd.h>
 /* portul folosit */
 #define PORT 2908
-#define NR_MAX_CLIENTI 3
-
 /* codul de eroare returnat de anumite apeluri */
 extern int errno;
-
-int valori[] = { 1,2,3,4,1,1,1,1,2,3 };
-int counter = 0;
-
-void displayValues(int valori[], int size) {
-  for(int item = 0; item < size; ++item){
-    printf("%d\n", valori[item]);
-  }
-}
-
-int chooseFavoriteFood(int arr[], int n, int k) { 
-  // Iterate though input array, for every element 
-    // arr[i], increment arr[arr[i]%k] by k 
-    for (int i = 0; i< n; i++) 
-        arr[arr[i]%k] += k; 
-  
-    // Find index of the maximum repeating element 
-    int max = arr[0], result = 0; 
-    for (int i = 1; i < n; i++) 
-    { 
-        if (arr[i] > max) 
-        { 
-            max = arr[i]; 
-            result = i; 
-        } 
-    } 
-  
-    /* Uncomment this code to get the original array back 
-       for (int i = 0; i< n; i++) 
-          arr[i] = arr[i]%k; */
-  
-    // Return index of the maximum element 
-    return result; 
-}
-
 typedef struct thData{
 	int idThread; //id-ul thread-ului tinut in evidenta de acest program
 	int cl; //descriptorul intors de accept
 }thData;
-
 static void *treat(void *); /* functia executata de fiecare thread ce realizeaza comunicarea cu clientii */
 void raspunde(void *);
-
 int main ()
 {
   struct sockaddr_in server;	// structura folosita de server
-  struct sockaddr_in from;	
-  int nr;		//mesajul primit de trimis la client 
-  int sd;		//descriptorul de socket 
+  struct sockaddr_in from;
+  int nr;		//mesajul primit de trimis la client
+  int sd;		//descriptorul de socket
   int pid;
   pthread_t th[100];    //Identificatorii thread-urilor care se vor crea
 	int i=0;
-  int nrMaxClienti = 0;
-
-  int rezult = chooseFavoriteFood(valori, sizeof(valori)/sizeof(valori[0]), 5 );
-
-  printf("the result %d\n", rezult);
-
   /* crearea unui socket */
   if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1)
     {
@@ -82,26 +40,23 @@ int main ()
   /* utilizarea optiunii SO_REUSEADDR */
   int on=1;
   setsockopt(sd,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(on));
-  
   /* pregatirea structurilor de date */
   bzero (&server, sizeof (server));
   bzero (&from, sizeof (from));
-  
   /* umplem structura folosita de server */
   /* stabilirea familiei de socket-uri */
-    server.sin_family = AF_INET;	
+    server.sin_family = AF_INET;
   /* acceptam orice adresa */
     server.sin_addr.s_addr = htonl (INADDR_ANY);
   /* utilizam un port utilizator */
     server.sin_port = htons (PORT);
-  
+
   /* atasam socketul */
   if (bind (sd, (struct sockaddr *) &server, sizeof (struct sockaddr)) == -1)
     {
       perror ("[server]Eroare la bind().\n");
       return errno;
     }
-
   /* punem serverul sa asculte daca vin clienti sa se conecteze */
   if (listen (sd, 2) == -1)
     {
@@ -109,80 +64,198 @@ int main ()
       return errno;
     }
   /* servim in mod concurent clientii...folosind thread-uri */
-  while(nrMaxClienti <= NR_MAX_CLIENTI) {
+  while (1)
+    {
       int client;
-      thData * td; //parametru functia executata de thread     
+      thData * td; //parametru functia executata de thread
       int length = sizeof (from);
 
       printf ("[server]Asteptam la portul %d...\n",PORT);
       fflush (stdout);
 
-      // client= malloc(sizeof(int));
       /* acceptam un client (stare blocanta pina la realizarea conexiunii) */
       if ( (client = accept (sd, (struct sockaddr *) &from, &length)) < 0)
-      {
-        perror ("[server]Eroare la accept().\n");
-        continue;
-      }
-      
-        /* s-a realizat conexiunea, se astepta mesajul */
-    
-      // int idThread; //id-ul threadului
-      // int cl; //descriptorul intors de accept
-
-      td=(struct thData*)malloc(sizeof(struct thData));	
-      td->idThread=i++;
-      td->cl=client;
-
-      pthread_create(&th[i], NULL, &treat, td);	      
-      nrMaxClienti++;
+	{
+	  perror ("[server]Eroare la accept().\n");
+	  continue;
 	}
-};				
+
+        /* s-a realizat conexiunea, se astepta mesajul */
+
+	int idThread; //id-ul threadului
+	int cl; //descriptorul intors de accept
+
+	td=(struct thData*)malloc(sizeof(struct thData));
+	td->idThread=i++;
+	td->cl=client;
+
+	pthread_create(&th[i], NULL, &treat, td);
+
+	}//while
+};
 static void *treat(void * arg)
-{		
-		struct thData tdL; 
-		tdL= *((struct thData*)arg);	
+{
+		struct thData tdL;
+		tdL= *((struct thData*)arg);
 		printf ("[thread]- %d - Asteptam mesajul...\n", tdL.idThread);
-		fflush (stdout);		 
-		pthread_detach(pthread_self());		
+		fflush (stdout);
+		pthread_detach(pthread_self());
 		raspunde((struct thData*)arg);
 		/* am terminat cu acest client, inchidem conexiunea */
 		close ((intptr_t)arg);
-		return(NULL);	
-  		
+		return(NULL);
+
 };
 
-
+// Console shopper
 void raspunde(void *arg) {
-    int nr, i=0;
-    struct thData tdL; 
-    tdL= *((struct thData*)arg);
-    if (read (tdL.cl, &nr,sizeof(int)) <= 0)
-        {
-          printf("[Thread %d]\n",tdL.idThread);
-          perror ("Eroare la read() de la client.\n");
-        
-        }
-    
-    printf ("[Thread %d]Mesajul a fost receptionat...%d\n",tdL.idThread, nr);
+  int len1, len2;
+	// Thread
+	struct thData tdL;
+	tdL= *((struct thData*)arg);
+  int operatie;
+  char nume[50], parola[50];
+	// Citim operatia
+  read(tdL.cl, &operatie, 4);
+  printf("Operatie : %d \n", operatie);
+  // Logare
+  if (operatie == 1) {
+      int cnt;
+      FILE *fis = fopen("cnt.txt", "r");
+      fscanf(fis, "%d", &cnt);
+      fclose(fis);
 
-    valori[counter++] = nr;
-    
-    for ( int i = 0; i < sizeof(valori) ; i++ ){
-      printf("valori si bla %d %d \n", i, valori[i]);
-    };
+			// Citim numele utilizatorului
+      read(tdL.cl, &len1, 4);
+      read(tdL.cl, nume, len1);
+      nume[len1] = '\0';
 
-    /*pregatim mesajul de raspuns */     
-    printf("[Thread %d]Trimitem mesajul inapoi...%d\n",tdL.idThread, nr);
-            
-            
-            /* returnam mesajul clientului */
-    if (write (tdL.cl, &nr, sizeof(int)) <= 0)
-      {
-      printf("[Thread %d] ",tdL.idThread);
-      perror ("[Thread]Eroare la write() catre client.\n");
+			// Citim parola
+      read(tdL.cl, &len2, 4);
+      read(tdL.cl, parola, len2);
+      parola[len2] = '\0';
+
+      printf("\n");
+      printf("%s %s", nume, parola);
+      printf("\n");
+      printf("%d %d", len1, len2);
+      printf("\n");
+      printf("CNT: %d", cnt);
+      printf("\n");
+
+      FILE *fis2 = fopen("useri.txt", "r");
+      int ok = 0;
+
+			// Cautam utilizatorul
+      for (int i = 0; i < cnt; i++){
+          char one[50], two[50];
+          fscanf(fis2, "%s", one);
+          fscanf(fis2, "%s", two);
+
+          if(strcmp(one, nume) == 0 && strcmp(two, parola) == 0){
+            ok = 1;
+          }
       }
-    else
-      printf ("[Thread %d]Mesajul a fost trasmis cu succes.\n",tdL.idThread);	
 
+			// Vedem daca s-a logat sau nu
+      write(tdL.cl, &ok, 4);
+  }
+
+  // Sign up - Aceeasi logica ca la Login pentru prima parte
+  if (operatie == 2) {
+      int cnt;
+      FILE *fis = fopen("cnt.txt", "r");
+      fscanf(fis, "%d", &cnt);
+      fclose(fis);
+
+      read(tdL.cl, &len1, 4);
+      read(tdL.cl, nume, len1);
+      nume[len1] = '\0';
+
+      read(tdL.cl, &len2, 4);
+      read(tdL.cl, parola, len2);
+      parola[len2] = '\0';
+
+      printf("\n");
+      printf("%s %s", nume, parola);
+      printf("\n");
+      printf("%d %d", len1, len2);
+      printf("\n");
+      printf("CNT: %d", cnt);
+      printf("\n");
+      FILE *fis2 = fopen("useri.txt", "r");
+      int ok = 0;
+
+			// Cautam sa vedem daca utilizatorul se afla in baza de date
+      for(int i = 0; i < cnt; i++){
+          char one[50], two[50];
+          fscanf(fis2, "%s", one);
+          fscanf(fis2, "%s", two);
+
+          if(strcmp(one, nume) == 0 && strcmp(two, parola) == 0){
+            ok = 1;
+          }
+      }
+
+      write(tdL.cl, &ok, 4);
+
+			// Daca nu am gasit utilizatorul, il bagam in baza de date
+      if (ok == 0){
+          fis = fopen("cnt.txt", "w");
+          cnt++;
+          fprintf(fis, "%d", cnt);
+          fclose(fis);
+          fis2 = fopen("useri.txt", "a+");
+          fprintf(fis2, "%s %s\n", nume, parola);
+          fclose(fis2);
+      }
+  }
+
+  int g = 1;
+
+  while (g) {
+    g = 0;
+    char categorii[5000];
+    memset(categorii, 0, sizeof(categorii));
+
+		// Afisam categoriile
+    strcat(categorii, "1.Imbracaminte.\n");
+    strcat(categorii, "2.Incaltaminte.\n");
+    strcat(categorii, "3.Telefonie.\n");
+    int len3 = strlen(categorii);
+    write(tdL.cl, &len3, 4);
+    write(tdL.cl, categorii, len3);
+    int cat;
+    read(tdL.cl, &cat, 4);
+    printf("Categorie : %d\n", cat);
+    FILE *fis3;
+
+    if (cat == 1) {
+      fis3 = fopen("imbracaminte.txt", "r");
+		} else if(cat == 2) {
+      fis3 = fopen("incaltaminte.txt", "r");
+		} else {
+      fis3 = fopen("telefonie.txt", "r");
+		}
+
+    int n;
+    char p[50][50];
+    char c[50][50];
+
+    fscanf(fis3, "%d", &n);
+    printf("N = %d\n", n);
+    for(int i = 0; i < n; i++){
+        fscanf(fis3, "%s %s",p[i], c[i]);
+    }
+    fclose(fis3);
+
+		// Afisam produsele (p, c vectorii care contin produsele)
+    write(tdL.cl, &n, 4);
+    printf("\n");
+    write(tdL.cl, p, 2500);
+    printf("\n");
+    write(tdL.cl, c, 2500);
+    printf("\n");
+    read(tdL.cl, &g, 4);
+  }
 }
